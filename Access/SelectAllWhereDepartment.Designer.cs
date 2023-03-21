6ccde9a1952318011c802de23125bcce39c07f6b
okayu <yuto.gio.0515@gamil.com>
2023-03-21 19:09:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Access/SelectAllWhereDepartment.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Access
+{
+    partial class SelectAllWhereDepartment
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewEmployees = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Employee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEmployees
+            // 
+            this.dataGridViewEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployees.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Employee,
+            this.Mail,
+            this.Department});
+            this.dataGridViewEmployees.Location = new System.Drawing.Point(71, 95);
+            this.dataGridViewEmployees.Name = "dataGridViewEmployees";
+            this.dataGridViewEmployees.RowTemplate.Height = 21;
+            this.dataGridViewEmployees.Size = new System.Drawing.Size(543, 150);
+            this.dataGridViewEmployees.TabIndex = 3;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Employee
+            // 
+            this.Employee.HeaderText = "Employee";
+            this.Employee.Name = "Employee";
+            // 
+            // Mail
+            // 
+            this.Mail.HeaderText = "Mail";
+            this.Mail.Name = "Mail";
+            this.Mail.Width = 200;
+            // 
+            // Department
+            // 
+            this.Department.HeaderText = "Department";
+            this.Department.Name = "Department";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(300, 24);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "検索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(69, 29);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(29, 12);
+            this.lblDepartment.TabIndex = 5;
+            this.lblDepartment.Text = "部署";
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Location = new System.Drawing.Point(104, 26);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(100, 19);
+            this.txtDepartment.TabIndex = 6;
+            // 
+            // SelectAllWhereDepartment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 299);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dataGridViewEmployees);
+            this.Name = "SelectAllWhereDepartment";
+            this.Text = "Access接続";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SelectAllWhereDepartment_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridViewEmployees;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Employee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Department;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.TextBox txtDepartment;
+    }
+}
+
---
+++ Access/SelectAllWhereDepartment.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Access
+{
+    public partial class SelectAllWhereDepartment : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();    // データベース接続用オブジェクト
+        private OleDbDataAdapter dataAdapter = new OleDbDataAdapter(); // テーブル操作実行用オブジェクト
+        private OleDbCommand command = new OleDbCommand();             // クエリ格納用オブジェクト
+
+        private DataTable dataTable = new DataTable();
+
+        public SelectAllWhereDepartment()
+        {
+            InitializeComponent();
+            InitDB();
+        }
+
+        private void InitDB()
+        {
+            // データベースをオープン
+            connection.ConnectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Data\\Database1.accdb";
+            connection.Open();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // 直前の取得結果をクリア
+            dataGridViewEmployees.Rows.Clear();
+            dataTable.Clear();
+
+            string sql = "SELECT * FROM Employees WHERE Department = @Department";
+            command.CommandText = sql;
+            command.Connection = connection;
+
+            // SQLインジェクション対策
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@Department", txtDepartment.Text);
+
+            dataAdapter.SelectCommand = command;
+
+            // 実行
+            dataAdapter.Fill(dataTable);
+
+            // 取得結果表示
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                dataGridViewEmployees.Rows.Add(dr.ItemArray[0], dr.ItemArray[1], dr.ItemArray[2], dr.ItemArray[3]);
+            }
+            
+        }      
+
+        private void SelectAllWhereDepartment_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // データベースをクローズ
+            connection.Close();
+        }
+    }
+}
